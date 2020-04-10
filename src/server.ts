d09c19596780114dc7ddb34673dba3ce7058a74d
OsirisFrik <osiris@writecode.studio>
2020-04-10 12:13:12 -0700

    ident

--- src/server.ts
+++ src/server.ts
@@ -13,44 +13,46 @@ dotenv.config()
 
 // Server Class
 class Server {
-    public app: express.Application;
+  public app: express.Application;
 
-    constructor() {
-        this.app = express();
-        this.config();
-        this.routes();
-    }
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
 
-    public config(): void {
-        const MONGO_URI:string | undefined = process.env.MONGO_URI;
+  public config(): void {
+    const MONGO_URI: string | undefined = process.env.MONGO_URI;
 
-        if (typeof MONGO_URI === 'undefined') throw new Error('NO MONGO URI')
+    if (typeof MONGO_URI === 'undefined') throw new Error('NO MONGO URI')
 
-        connect(MONGO_URI);
-        // Settings
-        this.app.set('port', process.env.PORT || 3000);
-        // middlewares
-        this.app.use(morgan('dev'));
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({extended: false}));
-        this.app.use(helmet());
-        this.app.use(compression());
-        this.app.use(cors());
-    }
+    connect(MONGO_URI);
+    // Settings
+    this.app.set('port', process.env.PORT || 3000);
+    // middlewares
+    this.app.use(morgan('dev'));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({
+      extended: false
+    }));
+    this.app.use(helmet());
+    this.app.use(compression());
+    this.app.use(cors());
+  }
 
-    public routes(): void {
-        const router: express.Router = express.Router();
+  public routes(): void {
+    const router: express.Router = express.Router();
 
-        // this.app.use('/', indexRoutes);
-        // this.app.use('/api/users', UserRoutes);
-    }
+    // this.app.use('/', indexRoutes);
+    // this.app.use('/api/users', UserRoutes);
+  }
 
-    public start(): void {
-        const PORT = process.env.PORT || 3000
-        this.app.listen(PORT, () => {
-            console.log(`Server is listenning on port: ${process.env.PORT}`);
-        });
-    }
+  public start(): void {
+    const PORT = process.env.PORT || 3000
+    this.app.listen(PORT, () => {
+      console.log(`🚀Server is listenning on port: ${process.env.PORT}`);
+    });
+  }
 }
 
 const server = new Server();
