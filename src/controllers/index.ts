90d7a7657820fba975dbd0cef7b27c2c4138c47a
arkamis <miguelantonio_rodriguez@ucol.mx>
2020-04-16 21:34:05 -0500

    adding views and navigation

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,16 +2,50 @@ import { Request, Response, NextFunction } from "express";
 
 export default class IndexController{
 	public getInfoPage(req: Request, res: Response, next: NextFunction): void{
-		res.render('users', {
-			title: 'dashboard'
+		res.render('index', {
+			title: 'dashboard',
+			pageName: 'Dashboard'
 		});
 	}
 	public renderUserLogin(req: Request, res: Response){
-		console.log('Users login')
     res.render('login', {
       layout: 'login',
       title: 'login'
     });
-  }
+	}
+	
+	public renderBusinessReg(req: Request, res: Response){
+		res.render('busReg', {
+			title: 'Registra tu negocio',
+			layout: 'login'
+		})
+	}
+
+	public showProfile(req: Request, res: Response){
+		res.render('perfil', {
+			title: 'perfil',
+			pageName:'Mi Perfil'
+		})
+	}
+
+	public showSales(req: Request, res:Response): void{
+		res.render('tableSales', {
+			title: 'Ventas',
+			pageName: 'Compras'
+		})
+	}
+	public showProducts(req: Request, res: Response): void {
+		res.render('products', {
+			title: 'mis productos',
+			pageName: 'Productos'
+		})
+	}
+
+	public getInformationUser(req: Request, res: Response): void{
+		res.render('information', {
+			title: 'Informacion',
+			pageName: 'Información'
+		})
+	}
 
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -16,6 +16,10 @@ export default class indexRouter{
 	routes(): void{
 		this.router.get('/', this.indexController.getInfoPage);
 		this.router.get('/login', this.indexController.renderUserLogin);
-
+		this.router.get('/bus-register', this.indexController.renderBusinessReg);
+		this.router.get('/perfil', this.indexController.showProfile);
+		this.router.get('/sales', this.indexController.showSales);
+		this.router.get('/products', this.indexController.showProducts);
+		this.router.get('/info', this.indexController.getInformationUser);
 	}
 }
